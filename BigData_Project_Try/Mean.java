a0b3c7289754ed039dfc894af30851b6bbc369b9
Ali Asaf <aliasafpolat@hotmail.com>
2020-04-28 02:20:40 +0300

    Add files via upload

--- BigData_Project_Try/Mean.java
+++ BigData_Project_Try/Mean.java
@@ -1,9 +1,11 @@
+
 import java.io.IOException;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.FloatWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
@@ -12,7 +14,9 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
 
-public class Summation {
+public class Mean {
+	
+	min max = 0
 
 	public static void main(String[] args) throws Exception {
 		
@@ -22,26 +26,26 @@ public class Summation {
 		Path output = new Path(files[1]);
 
 		@SuppressWarnings("deprecation")
-		Job j = new Job(c, "summation");
-		j.setJarByClass(Summation.class);
-		j.setMapperClass(MapSummation.class);
-		j.setReducerClass(ReduceSummation.class);
+		Job j = new Job(c, "mean");
+		j.setJarByClass(Mean.class);
+		j.setMapperClass(MapMean.class);
+		j.setReducerClass(ReduceMean.class);
 		j.setOutputKeyClass(Text.class);
-		j.setOutputValueClass(IntWritable.class);
+		j.setOutputValueClass(FloatWritable.class);
 		FileInputFormat.addInputPath(j, input);
 		FileOutputFormat.setOutputPath(j, output);
 		System.exit(j.waitForCompletion(true) ? 0 : 1);
 	}
 
-	public static class MapSummation extends Mapper<LongWritable, Text, Text, IntWritable> {
+	public static class MapMean extends Mapper<LongWritable, Text, Text, FloatWritable> {
 
-		private final int numOfCols = 18; // kolon sayısı   // locationColumnID = 8 , passengersColumnId = 9
+		private final int numOfCols = 18;
+		private final int  locationColumnId = 8, fareAmountColumnId = 10;
 		private Text locationId = new Text(); // key 
-		private final int locationColumnId = 8;
-		private final int passengerCountColumnId = 3;
-		private static IntWritable pc; // value     passeng count 
+		private static FloatWritable fa; // value     hesap 
 
 		private String[] lineToList(String line, int num_of_cols) {
+			
 			String[] list = new String[num_of_cols];
 
 			line = line.substring(0, line.length() - 1);
@@ -49,41 +53,42 @@ public class Summation {
 
 			return list;
 		}
-		
-		protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-			
-            String[] rowList = lineToList(value.toString(), numOfCols);
-			String line = value.toString();
+
+		public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+
+			String[] rowList = lineToList(value.toString(), numOfCols);
 			String locId = rowList[locationColumnId];
-			Integer passengCount;
+			Float fare;
 			
-			try{
-				passengCount = new Integer(Integer.parseInt(rowList[passengerCountColumnId])); 
+			try{ 
+				fare = new Float(Float.parseFloat(rowList[fareAmountColumnId]));
 			}
 			catch(NumberFormatException e){
-				passengCount = 0;
+				fare = 0.0;
 			}
-			 
-			pc = new IntWritable(passengCount);
+			
+			
+			fa = new FloatWritable(fare);
 			
 			locationId.set(locId);
-			context.write(locationId,pc);
+			context.write(locationId,fa);
 			
-		
-        }		
+			
+		}
 
 	}
-					
-	public static class ReduceSummation extends Reducer<Text, IntWritable, Text, IntWritable> {
-							
-		public void reduce(Text locationId, Iterable<IntWritable> values, Context con) throws IOException, InterruptedException {
-			int sum = 0;
 
-			for (IntWritable value : values) {
+	public static class ReduceMean extends Reducer<Text, FloatWritable, Text, FloatWritable> {
+
+		public void reduce(Text locationId, Iterable<FloatWritable> values, Context con) throws IOException, InterruptedException {
+			float sum = 0, mean = 0;
+			int count = 0;
+			for (FloatWritable value : values) {
 				sum += value.get();
+				count++;
 			}
-			
-			con.write(locationId, new IntWritable(sum));
+			mean = sum / count;
+			con.write(locationId, new FloatWritable(mean));
 		}
 	}
 }
