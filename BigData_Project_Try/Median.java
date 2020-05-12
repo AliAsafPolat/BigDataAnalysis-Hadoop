f3e4687a1be3838593e2491ecf4ae6dbb98c5ef2
Ali Asaf <aliasafpolat@hotmail.com>
2020-05-12 18:06:38 +0300

    Update Median.java

--- BigData_Project_Try/Median.java
+++ BigData_Project_Try/Median.java
@@ -39,76 +39,86 @@ public class Median {
 		System.exit(j.waitForCompletion(true) ? 0 : 1);
 	}
 
+	// Seyahat edilen lokasyonlara göre bahşişlerin ortancasını bulma. 
 	public static class MapMedian extends Mapper<LongWritable, Text, Text, FloatWritable> {
 
-		private final int numOfCols = 18;
-		private final int  locationColumnId = 8, tipAmountColumnId = 13;
-		private Text locationId = new Text(); // key 
-		private static FloatWritable ta; // value     bahşiş 
+		private final int kolon_sayisi = 18; 			// Veriset içerisindeki toplam kolon sayısı.
+		private final int lokasyon_kolon_id = 8;
+		private final int bahsis_miktari_kolon_id = 13; // key,value ikilisine denk gelen kolon numaraları
+		private Text lokasyon_id = new Text(); 			// key - lokasyon id
+		private static FloatWritable ta; 				// value - bahşiş miktarı  (tip amount)
 
-		private String[] lineToList(String line, int num_of_cols) {
-			
-			String[] list = new String[num_of_cols];
+		// .csv dosyası içerisindeki bir satırı alır ve kolon sırasına göre diziye atıp döndürür.
+		private String[] satirListele(String satir, int kolonSayisi) {
 
-			line = line.substring(0, line.length() - 1);
-			list = line.split(",");
-
+			String[] list = new String[kolonSayisi];
+			satir = satir.substring(0, satir.length() - 1);
+			list = satir.split(",");
 			return list;
 		}
 
-		public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
 
-			String[] rowList = lineToList(value.toString(), numOfCols);
-			String locId = rowList[locationColumnId];
-			Float tip;
-			
-			try{ 
-				tip = new Float(Float.parseFloat(rowList[tipAmountColumnId]));
+		public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+			// Veri setindeki bir satırı alır.
+			String[] satirListesi = satirListele(value.toString(), kolon_sayisi);
+
+			// Verisetinden lokasyon id bilgisinin değişkene atanması. 
+			String locId = satirListesi[lokasyon_kolon_id]; 
+
+			Float bahsis; 	// Bahşiş miktarını tutan değişken 
+
+			try{
+				// Verisetinde String olarak tutulan bahsis değeri Float değerine cast edilir. 		
+				bahsis = new Float(Float.parseFloat(satirListesi[bahsis_miktari_kolon_id]));
 			}
 			catch(NumberFormatException e){
-				tip = new Float(0);
+				bahsis = new Float(0);
 			}
-			
-			
-			ta = new FloatWritable(tip);
-			
-			locationId.set(locId);
-			context.write(locationId,ta);
-			
-			
-		}
+			// Bahşiş miktarı FloatWritable olarak dönüştürülür.
+			ta = new FloatWritable(bahsis);
 
+			// Key değeri olarak lokasyon id atanır.	
+			lokasyon_id.set(locId);
+
+			// Elde edilen key - value değerleri context üzerine yazılır.
+			context.write(lokasyon_id,ta); 
+		}
 	}
 
+
+
+
 	public static class ReduceMedian extends Reducer<Text, FloatWritable, Text, FloatWritable> {
-		public void reduce(Text locationId, Iterable<FloatWritable> values, Context con) throws IOException, InterruptedException {
-			
-			float median;
-			int halfSize;
+		public void reduce(Text lokasyon_id, Iterable<FloatWritable> values, Context con) throws IOException, InterruptedException {
+
+			float median; 		// median değerinin tutulduğu değişken.
+			int ortanca_deger; 	// Ortanca değeri tutan indis.
 			List<Float> valueList = new ArrayList<Float>();
-			
+
 			for (FloatWritable value : values) {
 				valueList.add(value.get());
 			}
-			Collections.sort(valueList);
-			int size = valueList.size();
 
-			if (size % 2 == 0) {
-				halfSize = size / 2;
-				median = (valueList.get(halfSize - 1) + valueList.get(halfSize)) / 2;
-			} else {
-				halfSize = (size + 1) / 2;
-				median = valueList.get(halfSize - 1);
+			Collections.sort(valueList); // Bahşiş değerleri sıralanır.
+			int size = valueList.size(); // Listenin büyüklüğü alınır.
+
+			if (size % 2 == 0) { 		 // Listenin eleman sayısı çift ise :
+				ortanca_deger = size / 2;
+
+				// Ortadaki 2 elemanın ortalaması alınır ve medyan bulunur
+				median = (valueList.get(ortanca_deger - 1) + valueList.get(ortanca_deger)) / 2; 
+
+			} else { 					 // Listenin eleman sayısı tek ise :
+
+				ortanca_deger = (size + 1) / 2; 	 // Ortadaki elemanı gösteren indis.
+				median = valueList.get(ortanca_deger - 1);  // Ortadaki eleman medyan olarak bulunur.
 			}
-			con.write(locationId, new FloatWritable(median));
-			
-			
-			
+
+			// Elde edilen değerler context üzerine yazılır.
+			con.write(lokasyon_id, new FloatWritable(median)); // key - value ikilisinin yazılması						
 		}
 	}
-	
-	
-	
+
 	
 	
 	
